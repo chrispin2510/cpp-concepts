4b19a41b2684a3f13ff60fa521ba5d45ef2b8778
Chrispin Charles <chrispin.charles@outlook.com>
2021-02-13 02:24:48 +0530

    A proper way for handling exception

--- excp_throw.cpp
+++ excp_throw.cpp
@@ -4,16 +4,14 @@ using namespace std;
 
 int abc(int a, int b, int c)
 {
-    if (a <= 0 || b <= 0 || c <= 0)
-        throw "All parameters should be > 0";
     return a + b * c + b / c;       
 }
 
 int main(int argc, char** argv) 
 {
-    try { cout << abc(2,0,3); }
-    catch (char* e) {
-        cout << "Exception: " << e << endl;
+    try { cout << abc(2,4,0) << endl; }
+    catch (const exception& e) {
+        cout << "Exception raised: " << e.what() << endl;
     }
     return 0;
 }
