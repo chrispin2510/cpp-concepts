d058f1adccd1c373a3608014b8ea0a7ccb7dcf89
Chrispin Charles <chrispin.charles@outlook.com>
2021-02-13 02:19:01 +0530

    First try to catch exception

--- excp_throw.cpp
+++ excp_throw.cpp
@@ -6,11 +6,14 @@ int abc(int a, int b, int c)
 {
     if (a <= 0 || b <= 0 || c <= 0)
         throw "All parameters should be > 0";
-    return a + b * c + b / c;       // potential divide by 0 error when c = 0!
+    return a + b * c + b / c;       
 }
 
 int main(int argc, char** argv) 
 {
-    int a = 2, b = 1, c = 0;
-    cout << abc(a,b,c) << endl; // instance of 'char const*' will be thrown
+    try { cout << abc(2,0,3); }
+    catch (char* e) {
+        cout << "Exception: " << e << endl;
+    }
+    return 0;
 }
