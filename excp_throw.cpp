2a0a2588c685c610473df01ed8855a34bb61a633
Chrispin Charles <chrispin.charles@outlook.com>
2021-02-13 02:05:11 +0530

    Added a naive way to handle exception

--- excp_throw.cpp
+++ excp_throw.cpp
@@ -4,11 +4,13 @@ using namespace std;
 
 int abc(int a, int b, int c)
 {
+    if (a <= 0 || b <= 0 || c <= 0)
+        throw "All parameters should be > 0";
     return a + b * c + b / c;       // potential divide by 0 error when c = 0!
 }
 
 int main(int argc, char** argv) 
 {
     int a = 2, b = 1, c = 0;
-    cout << abc(a,b,c) << endl; // floating point excecption
+    cout << abc(a,b,c) << endl; // instance of 'char const*' will be thrown
 }
