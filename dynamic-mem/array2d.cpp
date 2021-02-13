c31e0cfceb63f4c23e2b9b663e9b21b3eff6d99f
Chrispin Charles <chrispin.charles@outlook.com>
2021-02-13 18:42:23 +0530

    Made row size flexible - each row size can be specified independently

--- dynamic-mem/array2d.cpp
+++ dynamic-mem/array2d.cpp
@@ -5,28 +5,34 @@ using std::cin;  using std::bad_alloc;
 using std::endl;
 
 template <class T>
-void make2dArray(T ** &x, int numOfRows, int numOfCols)
+void makeArray(T * &x, int numItems)
 {
-    x = new T * [numOfRows];
-    for (int i = 0; i < numOfRows; i++) {
-        x[i] = new T [numOfCols];
+    x = new T [numItems];
+}
+
+template <class T>
+void make2dArray(T ** &x, int numRows, int * rowSize)
+{
+    x = new T * [numRows];
+    for (int i = 0; i < numRows; i++) {
+        x[i] = new T [rowSize[i]];
     }
 }
 
 template <class T>
-void init2dArray(T ** &x, int numOfRows, int numOfCols)
+void init2dArray(T ** &x, int numRows, int * rowSize)
 {
-    for (int i = 0; i < numOfRows; i++) {
-        for (int j = 0; j < numOfCols; j++) {
+    for (int i = 0; i < numRows; i++) {
+        for (int j = 0; j < rowSize[i]; j++) {
             cin >> x[i][j];
         }
     }
 }
 
 template <class T>
-void delete2dArray(T ** &x, int numOfRows)
+void delete2dArray(T ** &x, int numRows)
 {
-    for (int i = 0; i < numOfRows; i++) {
+    for (int i = 0; i < numRows; i++) {
         delete [] x[i];
     }
     delete [] x;
@@ -34,11 +40,11 @@ void delete2dArray(T ** &x, int numOfRows)
 }
 
 template <class T>
-void display2dArray(T ** &x, int numOfRows, int numOfCols)
+void display2dArray(T ** &x, int numRows, int * rowSize)
 {
-    for (int i = 0; i < numOfRows; i++) {
-        for (int j = 0; j < numOfCols; j++) {
-            if (j > 0 && j < numOfCols) { cout << ", "; }
+    for (int i = 0; i < numRows; i++) {
+        for (int j = 0; j < rowSize[i]; j++) {
+            if (j > 0 && j < rowSize[i]) { cout << ", "; }
             cout << x[i][j]; 
         }
         cout << endl;
@@ -47,23 +53,34 @@ void display2dArray(T ** &x, int numOfRows, int numOfCols)
 
 int main() 
 {
-    int **x;
-    int numOfRows, numOfCols;
+    int **x = nullptr;
+    int *rowSize = nullptr;
+    int numRows;
 
     cout << "\nenter number of rows: ";
-    cin >> numOfRows;
-    cout << "\nenter number of cols: ";
-    cin >> numOfCols;
+    cin >> numRows;
 
+    // Create matrix
     try {
-        make2dArray(x, numOfRows, numOfCols);
+        makeArray<int>(rowSize, numRows);
     }
     catch (bad_alloc) {
-        cerr << "Matrix could not be created" << endl;
+        cerr << "Caught memory error" << endl; 
         exit(EXIT_FAILURE);
     }
-    init2dArray(x, numOfRows, numOfCols);
-    display2dArray(x, numOfRows, numOfCols);
-    delete2dArray(x, numOfRows);
+    for (int i = 0; i < numRows; i++) {
+        cout << "enter size of row " << (i + 1) << " : ";
+        cin >> rowSize[i]; 
+    }
+    try {
+        make2dArray<int>(x, numRows, rowSize);
+    }
+    catch (bad_alloc) {
+        cerr << "Could not create matrix." << endl;
+        exit(EXIT_FAILURE);
+    }
+    init2dArray<int>(x, numRows, rowSize);
+    display2dArray<int>(x, numRows, rowSize);
+    delete2dArray<int>(x, numRows);
     return 0;
 }
